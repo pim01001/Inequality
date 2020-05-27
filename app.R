ad726e588273e34ee7795c38fcadf6fbc841be63
Pial <pialmirdha@gmail.com>
2020-05-27 19:18:22 -0400

    small changes to density plots

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shiny)
 library(maps)
 library(mapproj)
 library(dplyr)
+library(ggplot2)
 
 # Load data ----
 counties <- read.csv('HS.csv',stringsAsFactors = FALSE)
@@ -30,13 +31,21 @@ ui <- fluidPage(
                   value = c(0, 100))
     ),
     
-    mainPanel(plotOutput("map"))
+    mainPanel(
+      
+      tabsetPanel(type = "tabs",
+                  tabPanel("Map", plotOutput("map")),
+                  tabPanel("Density Plot", plotOutput("density")),
+                  tabPanel("Table", tableOutput("table"))
+        )
+      
+      )
   )
 )
 
 # Server logic ----
 server <- function(input, output) {
-   output$map <- renderPlot({
+  output$map <- renderPlot({
     data <- switch(input$var, 
                    "Heart & Stroke"=counties$Value )
     
@@ -48,6 +57,18 @@ server <- function(input, output) {
     
     percent_map(data, color, legend, input$range[1], input$range[2])
   })
+  
+  # density plot q
+ 
+  
+  output$density <- renderPlot({ 
+    
+    ggplot(counties,aes(Value,fill=TRUE))+
+      geom_vline(xintercept = mean(counties$Value,na.rm = TRUE),linetype='dashed',color='blue',size=2) +
+      geom_density(size=1,alpha=0.1)+
+      scale_color_brewer(palette="Paired") + theme_classic()
+    },height = 400,width = 600)
+  
 }
 
 # Run app ----
