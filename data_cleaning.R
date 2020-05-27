8d0d16222c62447b4e8f6abe9d2b0af8e5f72f15
Pial <pialmirdha@gmail.com>
2020-05-26 20:19:58 -0400

    folded helper into data cleaning for debug

--- data_cleaning.R
+++ data_cleaning.R
@@ -19,3 +19,51 @@ tt.df <- read.csv('/home/pim01001/Documents/Bootcamp/R/Shiny_test/test1/data/dat
 
 ll.df<-left_join(tt.df,HS.df,by='name')
 
+ll.df<-select(ll.df,name,Value)
+
+write.csv(ll.df,'/home/pim01001/Documents/Bootcamp/R/shiny_proj/HS.csv')
+
+
+percent_map <- function(var, color, legend.title, min = 0, max = 100) {
+  
+  # generate vector of fill colors for map
+  shades <- colorRampPalette(c("white", color))(100)
+  
+  # constrain gradient to percents that occur between min and max
+  #var <- pmax(var, min)
+  #var <- pmin(var, max)
+  
+  
+  percents <- as.integer(cut(var, 100, 
+                             include.lowest = TRUE, ordered = TRUE))
+  fills <- shades[percents]
+  #min <- min(percents,na.rm = TRUE)
+  #max <- max(percents,na.rm = TRUE)
+  # plot choropleth map
+  map("county", fill = TRUE, col = fills, 
+      resolution = 0, lty = 0, projection = "polyconic", 
+      myborder = 0, mar = c(0,0,0,0))
+  
+  # overlay state borders
+  map("state", col = "white", fill = FALSE, add = TRUE,
+      lty = 1, lwd = 1, projection = "polyconic", 
+      myborder = 0, mar = c(0,0,0,0))
+  
+  # add a legend
+  inc <- (max - min) / 4
+  legend.text <- c(paste0(min, " % or less"),
+                   paste0(min + inc, " %"),
+                   paste0(min + 2 * inc, " %"),
+                   paste0(min + 3 * inc, " %"),
+                   paste0(max, " % or more"))
+  
+  legend("bottomleft", 
+         legend = legend.text, 
+         fill = shades[c(1, 25, 50, 75, 100)], 
+         title = legend.title)
+}
+
+percent_map(ll.df$Value, 'darkviolet', "HS data", min = 25, max = 100)
+
+
+
