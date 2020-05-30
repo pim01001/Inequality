a9761fee4fc746505e92d6a3dccbfbcceafd7330
Pial <pialmirdha@gmail.com>
2020-05-30 12:34:19 -0400

    got HS data to map

--- leaf_test.R
+++ leaf_test.R
@@ -2,6 +2,8 @@ library(leaflet)
 library(dplyr)
 
 pop.df <- read.csv('/home/pim01001/Documents/Bootcamp/R/Shiny_test/geo.csv')
+HS.df <- read.csv('/home/pim01001/Documents/Bootcamp/R/shiny_proj/HS.csv')
+
 
 # m <- leaflet() %>% addTiles() %>% addMarkers(lng=174.768,lat=-36.852,
 #     popup='The birthplace of R')
@@ -17,6 +19,18 @@ m<-leaflet(pop2.df) %>% addTiles() %>%
              popup = ~paste(county,':',pop_county))
 
 print(m)
+
+#------------------------------------------------------------------
+HS.df <- HS.df %>% group_by(state,county) %>% 
+  summarise(Long = median(longitude),Lat= median(latitude),
+            HS_county = median(Value,na.rm=TRUE))
+
+u<-leaflet(HS.df) %>% addTiles() %>%
+  addCircles(lng = ~Long, lat = ~Lat, weight = 1,
+             radius = ~exp(HS_county*.011) , 
+             popup = ~paste(county,':',HS_county))
+
+print(u)
 
 # m<-leaflet(pop.df) %>% addTiles() %>%
 #   addCircles(lng = ~longitude, lat = ~latitude, weight = 1,
